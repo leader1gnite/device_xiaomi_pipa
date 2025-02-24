471d8e5ed7d361ccf7f80c5b42d13a8ae79ac9c3
Social <68400635+leader1gnite@users.noreply.github.com>
2025-02-24 19:39:29 +0200

    Import remove packages from YumeMichi's repository and adapt it to PixelOs

---
+++ remove_packages/Android.mk
@@ -0,0 +1,28 @@
+#
+# Copyright (C) 2023 Paranoid Android
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := RemovePackages
+LOCAL_MODULE_CLASS := APPS
+LOCAL_MODULE_TAGS := optional
+LOCAL_OVERRIDES_PACKAGES := \
+   CarrierServices \
+   com.google.android.dialer.support \
+   Contacts \
+   Dialer \
+   GoogleContacts \
+   GoogleDialer \
+   Messages \
+   messaging \
+   Mms \
+   PersonalSafety \
+   TeleService
+LOCAL_UNINSTALLABLE_MODULE := true
+LOCAL_CERTIFICATE := PRESIGNED
+LOCAL_SRC_FILES := /dev/null
+include $(BUILD_PREBUILT)
